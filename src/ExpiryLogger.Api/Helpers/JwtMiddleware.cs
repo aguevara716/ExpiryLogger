194a1877372e738cfd97285e44825bf6f033f5e0
Angel Guevara, Jr <aguevara.716@gmail.com>
2022-05-13 17:56:43 -0400

    Code cleanup

--- src/ExpiryLogger.Api/Helpers/JwtMiddleware.cs
+++ src/ExpiryLogger.Api/Helpers/JwtMiddleware.cs
@@ -9,13 +9,13 @@ namespace ExpiryLogger.Api.Helpers;
 
 public class JwtMiddleware
 {
-    private readonly RequestDelegate _next;
+    private readonly RequestDelegate _requestDelegate;
     private readonly AppSettings _appSettings;
     private readonly Dictionary<string, User> _userCache;
 
-    public JwtMiddleware(RequestDelegate next, IOptions<AppSettings> appSettings)
+    public JwtMiddleware(RequestDelegate requestDelegate, IOptions<AppSettings> appSettings)
     {
-        _next = next;
+        _requestDelegate = requestDelegate;
         _appSettings = appSettings.Value;
         _userCache = new Dictionary<string, User>();
     }
@@ -25,22 +25,22 @@ public class JwtMiddleware
         var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
         if (token is not null)
             AttachUserToContext(context, userService, token);
-        await _next.Invoke(context);
+        await _requestDelegate.Invoke(context);
     }
 
     private void AttachUserToContext(HttpContext context, IUserService userService, string token)
     {
         try
         {
-            var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
-            tokenHandler.ValidateToken(token, new TokenValidationParameters
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            _ = tokenHandler.ValidateToken(token, new TokenValidationParameters
             {
                 ValidateIssuerSigningKey = true,
                 IssuerSigningKey = new SymmetricSecurityKey(key),
                 ValidateIssuer = false,
                 ValidateAudience = false,
-                // set ClockSkew to 0 so tokens expire exactly at token expiration time (instead of 5 minutes later)
                 ClockSkew = TimeSpan.Zero
             }, out SecurityToken validatedToken);
 
