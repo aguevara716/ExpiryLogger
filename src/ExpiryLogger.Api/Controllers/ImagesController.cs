69bedf264c11803dde5601ebd448757c466f707d
Angel Guevara, Jr <aguevara.716@gmail.com>
2022-05-16 20:58:04 -0400

    Added CRUD controllers for all entities

---
+++ src/ExpiryLogger.Api/Controllers/ImagesController.cs
@@ -0,0 +1,13 @@
+﻿using ExpiryLogger.DataAccessLayer.Entities;
+using ExpiryLogger.DataAccessLayer.Repositories;
+
+namespace ExpiryLogger.Api.Controllers;
+
+public class ImagesController : CrudControllerBase<Image>
+{
+    public ImagesController(IRepository<Image> repository)
+        : base(repository)
+    {
+
+    }
+}
---
+++ src/ExpiryLogger.Api/Controllers/ProductDetailsController.cs
@@ -0,0 +1,13 @@
+﻿using ExpiryLogger.DataAccessLayer.Entities;
+using ExpiryLogger.DataAccessLayer.Repositories;
+
+namespace ExpiryLogger.Api.Controllers;
+
+public class ProductDetailsController : CrudControllerBase<ProductDetail>
+{
+    public ProductDetailsController(IRepository<ProductDetail> repository)
+        : base(repository)
+    {
+
+    }
+}
