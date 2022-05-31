340e5f7368259ffc0d173c05e7ffcb57865ba2bb
Angel Guevara, Jr <aguevara.716@gmail.com>
2022-05-31 00:15:03 -0400

    Switched to file-scoped namespace

--- src/ExpiryLogger.NotifierService/EmailData.cs
+++ src/ExpiryLogger.NotifierService/EmailData.cs
@@ -1,62 +1,59 @@
-﻿using System;
-using ExpiryLogger.DataAccessLayer.Entities;
+﻿using ExpiryLogger.DataAccessLayer.Entities;
 
-namespace ExpiryLogger.NotifierService
+namespace ExpiryLogger.NotifierService;
+
+public class EmailData
 {
-	public class EmailData
-	{
-		public ProductDetail[] ExpiredThisMonth { get; private set; }
-		public ProductDetail[] ExpiringToday { get; private set; }
-		public ProductDetail[] ExpiringThisWeek { get; private set; }
-		public ProductDetail[] ExpiringThisMonth { get; private set; }
+    public ProductDetail[] ExpiredThisMonth { get; private set; }
+    public ProductDetail[] ExpiringToday { get; private set; }
+    public ProductDetail[] ExpiringThisWeek { get; private set; }
+    public ProductDetail[] ExpiringThisMonth { get; private set; }
 
-        private EmailData()
-        {
-            ExpiredThisMonth = Array.Empty<ProductDetail>();
-            ExpiringToday = Array.Empty<ProductDetail>();
-            ExpiringThisWeek = Array.Empty<ProductDetail>();
-            ExpiringThisMonth = Array.Empty<ProductDetail>();
-        }
-
-        public class Builder
-        {
-            private readonly EmailData emailData;
-
-            public Builder()
-            {
-                emailData = new EmailData();
-            }
-
-            public Builder SetExpiredThisMonth(IEnumerable<ProductDetail> productDetails)
-            {
-                emailData.ExpiredThisMonth = productDetails?.ToArray() ?? Array.Empty<ProductDetail>();
-                return this;
-            }
-
-            public Builder SetExpiringToday(IEnumerable<ProductDetail> productDetails)
-            {
-                emailData.ExpiringToday = productDetails.ToArray()?.ToArray() ?? Array.Empty<ProductDetail>();
-                return this;
-            }
-
-            public Builder SetExpiringThisWeek(IEnumerable<ProductDetail> productDetails)
-            {
-                emailData.ExpiringThisWeek = productDetails?.ToArray() ?? Array.Empty<ProductDetail>();
-                return this;
-            }
-
-            public Builder SetExpiringThisMonth(IEnumerable<ProductDetail> productDetails)
-            {
-                emailData.ExpiringThisMonth = productDetails?.ToArray() ?? Array.Empty<ProductDetail>();
-                return this;
-            }
-
-            public EmailData Build()
-            {
-                return emailData;
-            }
-        }
-
+    private EmailData()
+    {
+        ExpiredThisMonth = Array.Empty<ProductDetail>();
+        ExpiringToday = Array.Empty<ProductDetail>();
+        ExpiringThisWeek = Array.Empty<ProductDetail>();
+        ExpiringThisMonth = Array.Empty<ProductDetail>();
     }
-}
 
+    public class Builder
+    {
+        private readonly EmailData emailData;
+
+        public Builder()
+        {
+            emailData = new EmailData();
+        }
+
+        public Builder SetExpiredThisMonth(IEnumerable<ProductDetail> productDetails)
+        {
+            emailData.ExpiredThisMonth = productDetails?.ToArray() ?? Array.Empty<ProductDetail>();
+            return this;
+        }
+
+        public Builder SetExpiringToday(IEnumerable<ProductDetail> productDetails)
+        {
+            emailData.ExpiringToday = productDetails.ToArray()?.ToArray() ?? Array.Empty<ProductDetail>();
+            return this;
+        }
+
+        public Builder SetExpiringThisWeek(IEnumerable<ProductDetail> productDetails)
+        {
+            emailData.ExpiringThisWeek = productDetails?.ToArray() ?? Array.Empty<ProductDetail>();
+            return this;
+        }
+
+        public Builder SetExpiringThisMonth(IEnumerable<ProductDetail> productDetails)
+        {
+            emailData.ExpiringThisMonth = productDetails?.ToArray() ?? Array.Empty<ProductDetail>();
+            return this;
+        }
+
+        public EmailData Build()
+        {
+            return emailData;
+        }
+    }
+
+}
